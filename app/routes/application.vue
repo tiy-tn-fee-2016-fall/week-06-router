64894a252f5f9f7d4e8b609e4204c8051106e4a5
Ryan Tablada <ryan.tablada@gmail.com>
2016-11-01 11:52:33 -0500

    Basic routing based on window.location.pathname

--- app/routes/application.vue
+++ app/routes/application.vue
@@ -9,41 +9,31 @@
 
     <div class="main">
       <div class="container">
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Street</th>
-              <th>City</th>
-              <th>Zip</th>
-              <th>Popularity</th>
-              <th></th>
-            </tr>
-          </thead>
-
-          <tbody>
-            <tr v-for="item in lunchSpots">
-              <td>{{ item.name }}</td>
-              <td>{{ item.street }}</td>
-              <td>{{ item.city }}</td>
-              <td>{{ item.zip }}</td>
-              <td>{{ item.votes }}</td>
-              <td></td>
-            </tr>
-          </tbody>
-        </table>
+        <template v-if="path === '/'">
+          <index-page :lunchSpots="lunchSpots"></index-page>
+        </template>
+        <template v-else>
+          <h2>NEW PAGE</h2>
+        </template>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import IndexPage from './index.vue';
 const apiUrl = 'http://tiy-tn-class-api-fall-16.herokuapp.com/lunch-spots';
 
+
 export default {
+  components: {
+    IndexPage,
+  },
+
   data() {
     return {
       lunchSpots: [],
+      path: window.location.pathname,
     };
   },
 
---
+++ app/routes/index.vue
@@ -0,0 +1,42 @@
+<template lang="html">
+  <div class="">
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Street</th>
+          <th>City</th>
+          <th>Zip</th>
+          <th>Popularity</th>
+          <th></th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr v-for="item in lunchSpots">
+          <td>{{ item.name }}</td>
+          <td>{{ item.street }}</td>
+          <td>{{ item.city }}</td>
+          <td>{{ item.zip }}</td>
+          <td>{{ item.votes }}</td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['lunchSpots'],
+
+  data() {
+    return {
+    };
+  },
+
+  methods: {
+
+  },
+};
+</script>
