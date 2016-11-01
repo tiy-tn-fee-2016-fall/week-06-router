1736f6e188e0c2df6de573582b318c88cf0e3498
Ryan Tablada <ryan.tablada@gmail.com>
2016-11-01 11:29:24 -0500

    Application vue

--- app/index.js
+++ app/index.js
@@ -1,1 +1,24 @@
 import 'whatwg-fetch';
+
+// Import library deps
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+// Import OUR routes
+import  Application from './routes/application.vue';
+
+// Hooked in the Router to Vue.js
+Vue.use(VueRouter);
+
+const routes = [
+  // {
+  //   path: '/',
+  //   name: 'index',
+  //   component: Index,
+  // },
+];
+
+
+// Kinda the same as saying
+// const app = new Application({ el: '.app' });
+const app = new Vue({ ...Application }).$mount('.app');
---
+++ app/routes/application.vue
@@ -0,0 +1,63 @@
+<template lang="html">
+  <div class="">
+    <div class="top-nav">
+      <div class="container">
+        <a href="/">All Lunch Spots</a>
+      </div>
+    </div>
+
+    <div class="main">
+      <div class="container">
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Street</th>
+              <th>City</th>
+              <th>Zip</th>
+              <th>Popularity</th>
+              <th></th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr v-for="item in lunchSpots">
+              <td>{{ item.name }}</td>
+              <td>{{ item.street }}</td>
+              <td>{{ item.city }}</td>
+              <td>{{ item.zip }}</td>
+              <td>{{ item.votes }}</td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+const apiUrl = 'http://tiy-tn-class-api-fall-16.herokuapp.com/lunch-spots';
+
+export default {
+  data() {
+    return {
+      lunchSpots: [],
+    };
+  },
+
+  mounted() {
+    this.getLunchSpots();
+  },
+
+  methods: {
+    getLunchSpots() {
+      fetch(apiUrl)
+        .then((r) => r.json())
+        .then((lunchSpots) => {
+          this.lunchSpots = lunchSpots;
+        });
+    },
+  },
+};
+</script>
